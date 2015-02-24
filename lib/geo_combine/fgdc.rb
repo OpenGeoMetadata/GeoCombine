c85237c74000ce67ffe4bc7a58a72c8db67a427e
Jack Reed <pjreed@stanford.edu>
2015-02-24 15:33:55 -0800

    use gem relative paths

--- lib/geo_combine/fgdc.rb
+++ lib/geo_combine/fgdc.rb
@@ -8,14 +8,14 @@ module GeoCombine
     # Returns a Nokogiri::XSLT object containing the FGDC to GeoBlacklight XSL
     # @return [Nokogiri::XSLT]
     def xsl_geoblacklight
-      Nokogiri::XSLT(File.read('./lib/xslt/fgdc2geoBL.xsl'))
+      Nokogiri::XSLT(File.read(File.join(File.dirname(__FILE__), '../xslt/fgdc2geoBL.xsl')))
     end
 
     ##
     # Returns a Nokogiri::XSLT object containing the ISO19139 to HTML XSL
     # @return [Nokogiri:XSLT]
     def xsl_html
-      Nokogiri::XSLT(File.read('./lib/xslt/fgdc2html.xsl'))
+      Nokogiri::XSLT(File.read(File.join(File.dirname(__FILE__), '../xslt/fgdc2html.xsl')))
     end
   end
 end
