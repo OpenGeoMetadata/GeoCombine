fb7faff1eac26bf26702913309c393845c1807f1
Jack Reed <pjreed@stanford.edu>
2015-04-28 19:33:06 -0700

    add rspec html matchers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,9 +23,11 @@ require 'geo_combine'
 require 'fixtures/xml_docs'
 require 'fixtures/json_docs'
 require 'helpers'
+require 'rspec-html-matchers'
 
 RSpec.configure do |config|
   config.include Helpers
+  config.include RSpecHtmlMatchers
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
