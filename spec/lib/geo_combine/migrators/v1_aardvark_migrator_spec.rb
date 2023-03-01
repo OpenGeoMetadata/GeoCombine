99b2e0f6771b7d83ecacf412bf1afdaa5e3dd47b
Nick Budak <budak@stanford.edu>
2023-03-01 13:43:17 -0800

    Schema v1 to Aardvark migrator
    - Handle elements without crosswalk (via lookup tables)
    - Support migrating collections in dct_isPartOf_sm
    - Convert single to multivalued fields where appropriate
    - Retain custom fields and remove deprecated fields
    
    Closes #121

--- spec/lib/geo_combine/migrators/v1_aardvark_migrator_spec.rb
+++ spec/lib/geo_combine/migrators/v1_aardvark_migrator_spec.rb
@@ -6,17 +6,41 @@ RSpec.describe GeoCombine::Migrators::V1AardvarkMigrator do
   include JsonDocs
 
   describe '#run' do
-    it 'migrates keys' do
+    it 'migrates fields to new names and types' do
       input_hash = JSON.parse(full_geoblacklight)
-      # TODO: Note that this fixture has not yet been fully converted to
-      # aardvark. See https://github.com/OpenGeoMetadata/GeoCombine/issues/121
-      # for remaining work.
       expected_output = JSON.parse(full_geoblacklight_aardvark)
       expect(described_class.new(v1_hash: input_hash).run).to eq(expected_output)
     end
 
+    it 'removes deprecated fields' do
+      input_hash = JSON.parse(full_geoblacklight)
+      output = described_class.new(v1_hash: input_hash).run
+      expect(output.keys).not_to include(described_class::SCHEMA_FIELD_MAP.keys)
+      expect(output.keys).not_to include('dc_type_s')
+      expect(output.keys).not_to include('layer_geom_type_s')
+    end
+
+    it 'leaves custom fields unchanged' do
+      input_hash = JSON.parse(full_geoblacklight)
+      input_hash['custom_field'] = 'custom_value'
+      output = described_class.new(v1_hash: input_hash).run
+      expect(output['custom_field']).to eq('custom_value')
+    end
+
     context 'when the given record is already in aardvark schema' do
-      xit 'returns the record unchanged'
+      it 'returns the record unchanged' do
+        input_hash = JSON.parse(full_geoblacklight_aardvark)
+        expect(described_class.new(v1_hash: input_hash).run).to eq(input_hash)
+      end
+    end
+
+    context 'when the user supplies a mapping for collection names to ids' do
+      it 'converts the collection names to ids' do
+        input_hash = JSON.parse(full_geoblacklight)
+        collection_id_map = { 'Uganda GIS Maps and Data, 2000-2010' => 'stanford-rb371kw9607' }
+        output = described_class.new(v1_hash: input_hash, collection_id_map: collection_id_map).run
+        expect(output['dct_isPartOf_sm']).to eq(['stanford-rb371kw9607'])
+      end
     end
   end
 end
